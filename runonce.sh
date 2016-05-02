87b5012b5e4589adb2c0cd774d9c08ee62e8e4b0
thackray <thackray@mit.edu>
2016-05-02 13:44:39 -0400

    first commit

---
+++ runonce.sh
@@ -0,0 +1,33 @@
+## PBS directives 
+## nb, PBS directives begin '#PBS'
+
+#PBS -N ptest
+#PBS -l nodes=1:ppn=16
+#PBS -j oe
+#PBS -q xlong
+#PBS -M thackray@mit.edu
+
+
+
+source /etc/profile.d/modules.csh
+module add intel
+
+
+
+
+   limit  stacksize     2097152 kbytes
+   setenv KMP_STACKSIZE 209715200
+   setenv OMP_NUM_THREADS 16
+
+
+cd /home/thackray/GEOS-Chem-rundirname/ # cd to your run dir
+rm -f HEMCO.log
+rm -f logrun # clear pre-existing log files 
+
+#run #1
+time ./geos > logrun # time job; pipe output to log file 
+
+
+
+
+exit(0) # exit normally
