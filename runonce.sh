3f0ab038955cbc9a1c510fd22dac9bf691fc3870
thackray <thackray@mit.edu>
2016-05-16 12:10:51 -0400

    use fileserver for real runs

--- runonce.sh
+++ runonce.sh
@@ -20,7 +20,7 @@ module add intel
    setenv OMP_NUM_THREADS 16
 
 
-cd /home/thackray/GEOS-Chem-rundirname/ # cd to your run dir
+cd /net/fs03/d1/thackray/GEOS-Chem-rundirname/ # cd to your run dir
 rm -f HEMCO.log
 rm -f logrun # clear pre-existing log files 
 
